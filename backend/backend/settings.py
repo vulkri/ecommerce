36e6bc6394319e5757c5cce39f31d15101e0650c
vulkri <kamil.pogoda2+github@gmail.com>
2024-05-14 12:06:32 +0200

    django connected with elasticsearch

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -209,7 +209,7 @@ THUMBNAILS = {
 }
 
 
-# Redis settings
+# Redis settings - need refactor as I opted for only-docker builds
 
 REDIS_HOST = os_getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os_getenv('REDIS_PORT', 6379)
@@ -238,6 +238,14 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_TIMEZONE = 'UTC'
 CELERY_RESULT_EXTENDED = True
+
+# Elasticsearch settings
+ELASTICSEARCH_DSL={
+    'default': {
+        'hosts': 'https://es01:9200',
+        'http_auth': ('elastic', os_getenv('ELASTIC_PASSWORD'))
+    }
+}
 
 # SMTP Settings
 
